58acaaced482c080f70e36441121fd04a9109695
马双飞 <mashuangfei@meituan.com>
2019-01-31 15:23:32 +0800

    add trees

---
+++ src/leetcode/Trees.Convert Sorted Array to Binary Search Tree.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    if (nums.isEmpty()) {
+        return null
+    }
+    return geneBST(nums, 0, nums.size - 1)
+}
+
+fun geneBST(nums: IntArray, start: Int, end: Int): TreeNode? {
+    if (end < start) {
+        return null
+    }
+    val mid = (start + end) shr 1
+
+    val root = TreeNode(nums[mid])
+    root.left = geneBST(nums, start, mid - 1)
+    root.right = geneBST(nums, mid + 1, end)
+    return root
+}
