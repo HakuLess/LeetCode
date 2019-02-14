1b8c250c1b4c5493515391e9c9b787a6274166da
马双飞 <mashuangfei@meituan.com>
2019-02-14 19:50:23 +0800

    add

---
+++ src/leetcode/942. DI String Match.kt
@@ -0,0 +1,33 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    diStringMatch("IDID").forEach {
+        print("$it, ")
+    }
+}
+
+fun diStringMatch(S: String): IntArray {
+    val ans = IntArray(S.length + 1)
+
+    var ptr = 0
+    for (i in 0 until S.length) {
+        if (S[i] == 'I') {
+            ans[i] = ptr
+            ptr++
+            println("$i ${ans[i]}")
+        }
+    }
+
+    ans[S.length] = ptr
+    ptr++
+    for (i in S.length - 1 downTo 0) {
+        if (S[i] == 'D') {
+            ans[i] = ptr
+            ptr++
+            println("$i ${ans[i]}")
+        }
+    }
+
+
+    return ans
+}
