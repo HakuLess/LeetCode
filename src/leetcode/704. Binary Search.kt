1df81a8d9bd7a556aecfecdc62361d169c7db53b
马双飞 <mashuangfei@meituan.com>
2019-03-04 13:43:18 +0800

    add

--- src/leetcode/704. Binary Search.kt
+++ src/leetcode/704. Binary Search.kt
@@ -1,3 +1,5 @@
+package leetcode
+
 class Solution704 {
     fun search(nums: IntArray, target: Int): Int {
         var left = 0
---
+++ src/leetcode/931. Minimum Falling Path Sum.kt
@@ -0,0 +1,33 @@
+package leetcode
+
+class Solution931 {
+
+    fun minFallingPathSum(A: Array<IntArray>): Int {
+        if (A.isEmpty() || A[0].isEmpty()) {
+            return 0
+        }
+        val x = A.size
+        val y = A[0].size
+
+        // falling lines and index
+        val matrix = Array(x) { IntArray(y) }
+        for (i in 0 until y) {
+            matrix[0][i] = A[0][i]
+        }
+
+        for (i in 1 until x) {
+            for (j in 0 until y) {
+                matrix[i][j] = matrix[i - 1][j]
+                if (j > 0) {
+                    matrix[i][j] = minOf(matrix[i - 1][j - 1], matrix[i][j])
+                }
+                if (j < y - 1) {
+                    matrix[i][j] = minOf(matrix[i - 1][j + 1], matrix[i][j])
+                }
+                matrix[i][j] += A[i][j]
+            }
+        }
+
+        return matrix[x - 1].min()!!
+    }
+}
