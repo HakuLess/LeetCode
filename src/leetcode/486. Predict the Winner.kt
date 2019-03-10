daa71e51d7ea5859927bd1a6ceac9aa9bd3587a4
马双飞 <mashuangfei@meituan.com>
2019-03-10 10:12:42 +0800

    add

--- src/leetcode/486. Predict the Winner.kt
+++ src/leetcode/486. Predict the Winner.kt
@@ -1,32 +1,50 @@
 package leetcode
 
-import kotlin.math.max
+fun main(args: Array<String>) {
+    val s = Solution486()
+    s.PredictTheWinner(intArrayOf(1, 5, 2)).print()
+}
 
-//todo didn't finish
-fun PredictTheWinner(nums: IntArray): Boolean {
+class Solution486 {
 
-    val matrix = Array(nums.size) {
-        IntArray(nums.size)
-    }
+    fun PredictTheWinner(nums: IntArray): Boolean {
+        val n = nums.size
+        val sum = nums.sum()
 
-    for (i in 0 until nums.size) {
-        for (j in i until nums.size) {
-            when {
-                j - i > 1 -> {
-                    matrix[i][j] = max(matrix.get(i + 2, j) + nums[i], matrix.get(i, j + 2) + nums[j])
-                }
-                j == i -> matrix[i][j] = nums[i]
-                else -> {
-                    matrix[i][j] = max(nums[i], nums[j])
+        val dp = Array(n) { IntArray(n) }
+        for (j in 0 until n) {
+            var curSum = 0
+            for (i in j downTo 0) {
+                curSum += nums[i]
+                if (i == j) {
+                    dp[i][j] = nums[i]
+                } else {
+                    dp[i][j] = maxOf(curSum - dp[i][j - 1], curSum - dp[i + 1][j])
                 }
             }
         }
-    }
-    return matrix[0][nums.size - 1] > nums.sum() / 2
-}
 
-fun Array<IntArray>.get(i: Int, j: Int): Int {
-    return this.getOrElse(i) {
-        IntArray(j + 1)
-    }[j]
+        dp.forEach {
+            it.forEach {
+                print("$it, ")
+            }
+            println()
+        }
+
+        return dp[0][n - 1] * 2 >= sum
+    }
+
+//    fun PredictTheWinner(nums: IntArray): Boolean {
+//        return getMax(nums, 0, nums.size - 1) >= 0
+//    }
+//
+//    fun getMax(nums: IntArray, start: Int, end: Int): Int {
+//        if (start == end) {
+//            return nums[start]
+//        }
+//
+//        val first = nums[start] - getMax(nums, start + 1, end)
+//        val last = nums[end] - getMax(nums, start, end - 1)
+//        return maxOf(first, last)
+//    }
 }
