464271f0de7bc1adaa103cc81da44bef2b5565fc
马双飞 <mashuangfei@meituan.com>
2019-03-17 13:01:10 +0800

    add

---
+++ src/leetcode/1013. Pairs of Songs With Total Durations Divisible by 60.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    val s = Solution1013()
+    s.numPairsDivisibleBy60(intArrayOf(30, 20, 150, 100, 40)).print()
+}
+
+class Solution1013 {
+    fun numPairsDivisibleBy60(time: IntArray): Int {
+        var ans = 0
+        for (i in 0 until time.size - 1) {
+            for (j in i + 1 until time.size) {
+                if ((time[i] + time[j]) % 60 == 0) {
+                    ans++
+                }
+            }
+        }
+        return ans
+    }
+}
