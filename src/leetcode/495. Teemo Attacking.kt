1938583454d0d917c39f4c6eac07a7c004fb7595
马双飞 <mashuangfei@meituan.com>
2019-03-21 17:05:05 +0800

    add

---
+++ src/leetcode/495. Teemo Attacking.kt
@@ -0,0 +1,31 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    val s = Solution495()
+    s.findPoisonedDuration(intArrayOf(1, 4), 2).print()
+    s.findPoisonedDuration(intArrayOf(1, 2), 2).print()
+}
+
+class Solution495 {
+    fun findPoisonedDuration(timeSeries: IntArray, duration: Int): Int {
+        if (timeSeries.isEmpty()) {
+            return 0
+        }
+        var ans = 0
+        var start = timeSeries[0]
+        var end = start + duration
+
+        for (i in 1 until timeSeries.size) {
+            if (timeSeries[i] <= end) {
+                end = timeSeries[i] + duration
+            } else {
+                ans += end - start
+                start = timeSeries[i]
+                end = timeSeries[i] + duration
+            }
+        }
+        ans += end - start
+
+        return ans
+    }
+}
---
+++ src/leetcode/932. Beautiful Array.kt
@@ -0,0 +1,30 @@
+package leetcode
+
+import java.util.ArrayList
+
+
+fun main(args: Array<String>) {
+    val s = Solution932()
+    s.beautifulArray(5).print()
+    s.beautifulArray(7).print()
+}
+//todo study
+
+class Solution932 {
+//    fun beautifulArray(N: Int): IntArray {
+//        return intArrayOf()
+//    }
+
+    fun beautifulArray(N: Int): IntArray {
+        var res = ArrayList<Int>()
+        res.add(1)
+        while (res.size < N) {
+            val tmp = ArrayList<Int>()
+            for (i in res) if (i * 2 - 1 <= N) tmp.add(i * 2 - 1)
+            for (i in res) if (i * 2 <= N) tmp.add(i * 2)
+            res = tmp
+        }
+//        return res.stream().mapToInt { i -> i }.toArray()
+        return res.toIntArray()
+    }
+}
