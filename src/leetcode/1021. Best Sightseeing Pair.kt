b4e1e785327bb62a6fc18b03b3941d759137fadc
马双飞 <mashuangfei@meituan.com>
2019-03-24 10:15:22 +0800

    add

---
+++ src/leetcode/1021. Best Sightseeing Pair.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    val s = Solution1021()
+    s.maxScoreSightseeingPair(
+            intArrayOf(8, 1, 5, 2, 6)
+    ).print()
+}
+
+class Solution1021 {
+    fun maxScoreSightseeingPair(A: IntArray): Int {
+        var max = 0
+        for (i in 0 until A.size - 1) {
+            for (j in i + 1 until A.size) {
+                max = maxOf(A[j] + A[i] + i - j, max)
+            }
+        }
+        return max
+    }
+}
---
+++ src/leetcode/1022. Smallest Integer Divisible by K.kt
@@ -0,0 +1,34 @@
+package leetcode
+
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+    val s = Solution1022()
+//    s.smallestRepunitDivByK(3).print()
+//    s.smallestRepunitDivByK(1).print()
+//    s.smallestRepunitDivByK(2).print()
+    s.smallestRepunitDivByK(5).print()
+    s.smallestRepunitDivByK(17).print()
+    s.smallestRepunitDivByK(23).print()
+    s.smallestRepunitDivByK(18367).print()
+    s.smallestRepunitDivByK(99999).print()
+    s.smallestRepunitDivByK(19927).print()
+}
+
+class Solution1022 {
+    fun smallestRepunitDivByK(K: Int): Int {
+        if (K % 5 == 0 || K % 2 == 0) {
+            return -1
+        }
+        var sum = BigInteger.valueOf(1L)
+        for (i in 0 until 30000) {
+//            println(sum)
+            if (sum % BigInteger.valueOf(K.toLong()) == BigInteger.ZERO) {
+                return i + 1
+            }
+            sum = sum.multiply(BigInteger.valueOf(10)).add(BigInteger.valueOf(1))
+        }
+
+        return -1
+    }
+}
