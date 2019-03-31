e49d1416a1e2fab6f755dc68d5d8dc70c7e3d5af
马双飞 <mashuangfei@meituan.com>
2019-03-31 10:29:38 +0800

    add

---
+++ src/leetcode/501. Find Mode in Binary Search Tree.kt
@@ -0,0 +1,32 @@
+package leetcode
+
+import java.util.*
+
+class Solution501 {
+    fun findMode(root: TreeNode?): IntArray {
+        if (root == null) {
+            return intArrayOf()
+        }
+
+        val map = hashMapOf<Int, Int>()
+        val queue: Queue<TreeNode?> = LinkedList<TreeNode>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            for (i in 0 until size) {
+                val item = queue.poll() ?: continue
+                map[item.`val`] = map.getOrDefault(item.`val`, 0) + 1
+                queue.add(item.left)
+                queue.add(item.right)
+            }
+        }
+        val max = map.values.max()!!
+        val ans = arrayListOf<Int>()
+        map.forEach { t, u ->
+            if (max == u) {
+                ans.add(t)
+            }
+        }
+        return ans.toIntArray()
+    }
+}
---
+++ src/leetcode/594. Longest Harmonious Subsequence.kt
@@ -0,0 +1,26 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    val s = Solution594()
+    s.findLHS(intArrayOf(1, 3, 2, 2, 5, 2, 3, 7)).print()
+    s.findLHS(intArrayOf(1, 1, 1, 1)).print()
+}
+
+class Solution594 {
+    fun findLHS(nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+        val map = hashMapOf<Int, Int>()
+        for (i in 0 until nums.size) {
+            map[nums[i]] = map.getOrDefault(nums[i], 0) + 1
+        }
+        var max = 0
+        map.forEach { t, u ->
+            if (map.containsKey(t + 1)) {
+                max = maxOf(u + map[t + 1]!!, max)
+            }
+        }
+        return max
+    }
+}
