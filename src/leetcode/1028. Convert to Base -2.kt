9230a35fb95337c2f2f51b1276807b796b5ff21b
马双飞 <mashuangfei@meituan.com>
2019-03-31 12:35:09 +0800

    add

--- src/leetcode/1028. Convert to Base -2.kt
+++ src/leetcode/1028. Convert to Base -2.kt
@@ -4,45 +4,65 @@ import java.lang.StringBuilder
 
 fun main(args: Array<String>) {
     val s = Solution1028()
-//    s.baseNeg2(6).print()
+    s.baseNeg2(6).print()
 //    s.baseNeg2(2).print()
 //    s.baseNeg2(3).print()
 //    s.baseNeg2(4).print()
-    s.baseNeg2(0).print()
-    s.baseNeg2(1).print()
+//    s.baseNeg2(0).print()
+//    s.baseNeg2(1).print()
 }
 
 class Solution1028 {
+
     fun baseNeg2(N: Int): String {
-        if (N == 0) {
-            return "0"
-        }
-        val str = Integer.toBinaryString(N)
-        val list = arrayListOf<Int>()
-        list.add(0)
-        list.add(0)
-        str.forEach {
-            list.add(it - '0')
-        }
+        var n = N
+        if (n == 0) return "0"
+        val sb = StringBuilder()
 
-        for (i in list.lastIndex downTo 0) {
-            if (list[i] == 2) {
-                list[i - 1]++
-                list[i] = 0
+        while (n > 0) {
+            val r = n % 2
+            sb.append(r)
+//            println("$r， $n")
+            n = if (sb.length % 2 == 0) {
+                println("$n + $r / 2")
+                (n + r) / 2
+            } else {
+                println("$n - $r / 2")
+                (n - r) / 2
             }
-            if ((list.lastIndex - i) % 2 != 0) {
-                println("$i, ${list[i]}")
-                if (list[i] == 1) {
-                    list[i - 1]++
-                }
-            }
-
         }
-
-        val ans = StringBuilder()
-        list.forEach {
-            ans.append(it)
-        }
-        return ans.trimStart('0').toString()
+        return sb.reverse().toString()
     }
+//    fun baseNeg2(N: Int): String {
+//        if (N == 0) {
+//            return "0"
+//        }
+//        val str = Integer.toBinaryString(N)
+//        val list = arrayListOf<Int>()
+//        list.add(0)
+//        list.add(0)
+//        str.forEach {
+//            list.add(it - '0')
+//        }
+//
+//        for (i in list.lastIndex downTo 0) {
+//            if (list[i] == 2) {
+//                list[i - 1]++
+//                list[i] = 0
+//            }
+//            if ((list.lastIndex - i) % 2 != 0) {
+//                println("$i, ${list[i]}")
+//                if (list[i] == 1) {
+//                    list[i - 1]++
+//                }
+//            }
+//
+//        }
+//
+//        val ans = StringBuilder()
+//        list.forEach {
+//            ans.append(it)
+//        }
+//        return ans.trimStart('0').toString()
+//    }
 }
