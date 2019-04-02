772487f76c5f1c9f35ea013bb293021383c58650
马双飞 <mashuangfei@meituan.com>
2019-04-02 14:12:57 +0800

    add

---
+++ src/leetcode/322. Coin Change.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+fun main(args: Array<String>) {
+    val s = Solution322()
+    s.coinChange(intArrayOf(1, 2, 5), 11).print()
+    s.coinChange(intArrayOf(2), 11).print()
+}
+
+class Solution322 {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val dp = IntArray(amount + 1) { -1 }
+        dp[0] = 0
+        for (i in 1 until dp.size) {
+            coins.forEach {
+                if (i - it in 0..dp.lastIndex && dp[i - it] >= 0) {
+                    if (dp[i] < 0) {
+                        dp[i] = dp[i - it] + 1
+                    } else {
+                        dp[i] = minOf(dp[i - it] + 1, dp[i])
+                    }
+                }
+            }
+            dp.print()
+        }
+        return dp[amount]
+    }
+}
