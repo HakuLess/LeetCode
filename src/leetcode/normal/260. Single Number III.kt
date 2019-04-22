aea8b42e4538f029c6d9a8e81618ca7d5a9def27
马双飞 <mashuangfei@meituan.com>
2019-04-22 10:05:04 +0800

    add

---
+++ src/leetcode/normal/260. Single Number III.kt
@@ -0,0 +1,18 @@
+package leetcode.normal
+
+class Solution260 {
+    fun singleNumber(nums: IntArray): IntArray {
+        val map = hashMapOf<Int, Int>()
+        nums.forEach {
+            map[it] = map.getOrDefault(it, 0) + 1
+        }
+
+        val ans = arrayListOf<Int>()
+        map.forEach { t, u ->
+            if (u == 1) {
+                ans.add(t)
+            }
+        }
+        return ans.toIntArray()
+    }
+}
