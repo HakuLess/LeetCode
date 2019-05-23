1b30dbcd19fa145b50afb20d66129192355b9403
马双飞 <mashuangfei@meituan.com>
2019-05-23 10:40:00 +0800

    add

---
+++ src/leetcode/normal/415. Add Strings.kt
@@ -0,0 +1,58 @@
+package leetcode.normal
+
+import java.lang.StringBuilder
+
+class Solution415 {
+    fun addStrings(num1: String, num2: String): String {
+        var isAdd = false
+        val l1 = num1.length
+        val l2 = num2.length
+        val s1 = if (l1 < l2) {
+            num1.reversed()
+        } else {
+            num2.reversed()
+        }
+        val s2 = if (l1 < l2) {
+            num2.reversed()
+        } else {
+            num1.reversed()
+        }
+
+        val sb = StringBuilder()
+        for (i in 0 until s1.length) {
+            val it = (s1[i] - '0') + (s2[i] - '0') + if (isAdd) {
+                1
+            } else {
+                0
+            }
+
+            isAdd = if (it >= 10) {
+                sb.append(it - 10)
+                true
+            } else {
+                sb.append(it)
+                false
+            }
+        }
+
+        for (j in s1.length until s2.length) {
+            val it = (s2[j] - '0') + if (isAdd) {
+                1
+            } else {
+                0
+            }
+            isAdd = if (it >= 10) {
+                sb.append(it - 10)
+                true
+            } else {
+                sb.append(it)
+                false
+            }
+        }
+        if (isAdd) {
+            sb.append('1')
+        }
+
+        return sb.toString().reversed()
+    }
+}
