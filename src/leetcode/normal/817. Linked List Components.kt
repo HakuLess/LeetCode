6dabf9f306a1e075915d9383b686d59394b0479a
马双飞 <mashuangfei@meituan.com>
2019-05-24 11:50:17 +0800

    add unfinished

---
+++ src/leetcode/normal/817. Linked List Components.kt
@@ -0,0 +1,10 @@
+package leetcode.normal
+
+import leetcode.ListNode
+
+class Solution817 {
+    fun numComponents(head: ListNode?, G: IntArray): Int {
+        var ans = 0
+        return ans
+    }
+}
