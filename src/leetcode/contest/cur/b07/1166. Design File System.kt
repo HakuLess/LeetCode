09054e83d9de623049295af3694b378646fabc4f
马双飞 <mashuangfei@meituan.com>
2019-08-25 10:25:30 +0800

    add

---
+++ src/leetcode/contest/cur/b07/1166. Design File System.kt
@@ -0,0 +1,36 @@
+package leetcode.contest.cur.b07
+
+class FileSystem() {
+
+    val map = hashMapOf<String, Int>()
+
+    fun create(path: String, value: Int): Boolean {
+        if (path.isEmpty()) {
+            return false
+        }
+        if (path.count { it == '/' } == 1) {
+            map[path] = value
+            return true
+        } else {
+            val sub = path.substring(IntRange(0, path.lastIndexOf('/') - 1))
+            println(sub)
+            if (map.containsKey(sub)) {
+                map[path] = value
+                return true
+            }
+        }
+        return false
+    }
+
+    fun get(path: String): Int {
+        return map[path] ?: -1
+    }
+
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * var obj = FileSystem()
+ * var param_1 = obj.create(path,value)
+ * var param_2 = obj.get(path)
+ */
---
+++ src/leetcode/contest/cur/b07/1168. Optimize Water Distribution in a Village.kt
@@ -0,0 +1,43 @@
+package leetcode.contest.cur.b07
+
+import leetcode.contest.utils.UFS
+import leetcode.contest.utils.print
+
+fun main(args: Array<String>) {
+    val s = Solution1168()
+    s.minCostToSupplyWater(3,
+            intArrayOf(1, 2, 2),
+            arrayOf(
+                    intArrayOf(1, 2, 1),
+                    intArrayOf(2, 3, 1)))
+            .print()
+
+    s.minCostToSupplyWater(5,
+            intArrayOf(46012, 72474, 64965, 751, 33304),
+            arrayOf(
+                    intArrayOf(2, 1, 6719),
+                    intArrayOf(3, 2, 75312),
+                    intArrayOf(5, 3, 44918)
+            )).print()
+}
+
+class Solution1168 {
+
+    fun minCostToSupplyWater(n: Int, wells: IntArray, pipes: Array<IntArray>): Int {
+        val ufs = UFS(n + 1)
+        val pipe = arrayListOf<IntArray>()
+        pipe.addAll(pipes)
+        wells.forEachIndexed { index, i ->
+            pipe.add(intArrayOf(0, index + 1, i))
+        }
+        var ans = 0
+        pipe.sortBy { it[2] }
+        pipe.forEach {
+            if (ufs.union(it[0], it[1])) {
+                println("add ${it[0]} & ${it[1]} is ${it[2]}")
+                ans += it[2]
+            }
+        }
+        return ans
+    }
+}
