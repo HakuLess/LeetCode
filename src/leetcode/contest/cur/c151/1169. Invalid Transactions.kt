c375a7f8abebe8ab90b5897630c19a5666c4991b
马双飞 <mashuangfei@meituan.com>
2019-08-26 11:02:52 +0800

    add

---
+++ src/leetcode/contest/cur/c151/1169. Invalid Transactions.kt
@@ -0,0 +1,51 @@
+package leetcode.contest.cur.c151
+
+import kotlin.math.abs
+
+fun main(args: Array<String>) {
+    val s = Solution1169()
+    s.invalidTransactions(
+//            arrayOf("alice,20,800,mtv", "alice,50,100,beijing")
+            arrayOf("bob,689,1910,barcelona",
+                    "alex,696,122,bangkok",
+                    "bob,832,1726,barcelona",
+                    "bob,820,596,bangkok",
+                    "chalicefy,217,669,barcelona",
+                    "bob,175,221,amsterdam")
+    ).forEach {
+        println(it)
+    }
+}
+
+class Solution1169 {
+    fun invalidTransactions(transactions: Array<String>): List<String> {
+        val ans = hashSetOf<String>()
+
+        ans.addAll(transactions.filter {
+            it.split(",")[2].toInt() > 1000
+        })
+
+        transactions.sortBy { it.split(",")[1] }
+
+        transactions.forEach {
+            if (!isValid(it, transactions)) {
+                ans.add(it)
+            }
+        }
+
+        return ans.toList()
+    }
+
+    private fun isValid(str: String, transactions: Array<String>): Boolean {
+        val n = str.split(",")[0]
+        val t = str.split(",")[1].toInt()
+        val c = str.split(",")[3]
+        if (transactions.any {
+                    val s = it.split(",")
+                    s[0] == n && s[3] != c && abs(s[1].toInt() - t) <= 60
+                }) {
+            return false
+        }
+        return true
+    }
+}
---
+++ src/leetcode/contest/cur/c151/1170. Compare Strings by Frequency of the Smallest Character.kt
@@ -0,0 +1,28 @@
+package leetcode.contest.cur.c151
+
+class Solution1170 {
+    fun numSmallerByFrequency(queries: Array<String>, words: Array<String>): IntArray {
+        val a = IntArray(queries.size)
+        val b = IntArray(words.size)
+
+        for (i in 0 until a.size) {
+            a[i] = f(queries[i])
+        }
+        for (i in 0 until b.size) {
+            b[i] = f(words[i])
+        }
+        val ans = arrayListOf<Int>()
+        a.forEach { t ->
+            ans.add(b.count { it > t })
+        }
+        return ans.toIntArray()
+    }
+
+    private fun f(s: String): Int {
+        var c = 'z'
+        s.forEach {
+            c = minOf(it, c)
+        }
+        return s.count { it == c }
+    }
+}
