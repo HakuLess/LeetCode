514841bc48c409cfb0add031cfa20a546b6a429d
马双飞 <mashuangfei@meituan.com>
2019-09-15 03:21:32 +0800

    add

---
+++ src/leetcode/contest/cur/1143. Longest Common Subsequence.kt
@@ -0,0 +1,9 @@
+package leetcode.contest.cur
+
+import leetcode.contest.utils.lcs
+
+class Solution1143 {
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        return lcs(text1.toList(), text2.toList(), text1.length, text2.length)
+    }
+}
