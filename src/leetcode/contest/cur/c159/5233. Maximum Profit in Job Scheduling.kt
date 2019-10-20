fd16daceec1b415683ce18092c72f4334fcddf24
马双飞 <mashuangfei@meituan.com>
2019-10-20 14:52:50 +0800

    add

--- src/leetcode/contest/cur/c159/5233. Maximum Profit in Job Scheduling.kt
+++ src/leetcode/contest/cur/c159/5233. Maximum Profit in Job Scheduling.kt
@@ -2,32 +2,24 @@ package leetcode.contest.cur.c159
 
 class Solution5233 {
 
-    private val map = HashMap<Int, Int>()
-
     fun jobScheduling(startTime: IntArray, endTime: IntArray, profit: IntArray): Int {
         val w = arrayListOf<Triple<Int, Int, Int>>()
         for (i in startTime.indices) {
             w.add(Triple(startTime[i], endTime[i], profit[i]))
         }
-        w.sortBy { it.first }
-        return helper(w, 0)
-    }
+        w.sortBy { it.second }
 
-    private fun helper(w: List<Triple<Int, Int, Int>>, now: Int): Int {
-        if (map.containsKey(now)) {
-            return map[now]!!
+        val dp = IntArray(w.size)
+        dp[0] = w[0].third
+        for (i in 1 until w.size) {
+            dp[i] = maxOf(w[i].third, dp[i - 1])
+            for (j in i - 1 downTo 0) {
+                if (w[j].second <= w[i].first) {
+                    dp[i] = maxOf(dp[i], w[i].third + dp[j])
+                    break
+                }
+            }
         }
-        val left = w.filter { it.first >= now }
-        if (left.isEmpty()) {
-            return 0
-        }
-
-        var res = 0
-        for (i in left.indices) {
-            res = maxOf(res, helper(left, left[i].second) + left[i].third)
-        }
-
-        map[now] = res
-        return res
+        return dp.max()!!
     }
 }
