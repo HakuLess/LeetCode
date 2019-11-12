814225ca99dc1445586e42d61365b0b1c3d4225a
马双飞 <mashuangfei@meituan.com>
2019-11-12 11:25:56 +0800

    add

--- src/leetcode/normal/33. Search in Rotated Sorted Array.kt
+++ src/leetcode/normal/33. Search in Rotated Sorted Array.kt
@@ -3,35 +3,38 @@ package leetcode.normal
 import leetcode.contest.utils.print
 
 fun main(args: Array<String>) {
-    search(intArrayOf(3), 3).print()
+    val s = Solution33()
+    s.search(intArrayOf(3), 3).print()
 }
 
-fun search1(nums: IntArray, target: Int): Int {
-    var left = 0
-    var right = nums.lastIndex
+class Solution33 {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.lastIndex
 
-    while (left <= right) {
-        val mid = left + (right - left) / 2
-        if (nums[mid] == target) {
-            return mid
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            if (nums[mid] == target) {
+                return mid
+            }
+
+            if (nums[mid] > nums[right]) {
+                // left is sorted
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else {
+                // right is sorted
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
         }
 
-        if (nums[mid] > nums[right]) {
-            // left is sorted
-            if (target >= nums[left] && target < nums[mid]) {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
-        } else {
-            // right is sorted
-            if (target > nums[mid] && target <= nums[right]) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-        }
+        return -1
     }
-
-    return -1
 }
