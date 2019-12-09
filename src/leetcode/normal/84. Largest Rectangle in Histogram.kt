4a200d585f2c5fad38e65ecd3152d37c36fb5c78
马双飞 <mashuangfei@meituan.com>
2019-12-09 14:30:18 +0800

    add

--- src/leetcode/normal/84. Largest Rectangle in Histogram.kt
+++ src/leetcode/normal/84. Largest Rectangle in Histogram.kt
@@ -1,44 +1,61 @@
 package leetcode.normal
 
-import kotlin.math.max
+import java.util.*
 
 /**
  * Created by HaKu on 27/02/2018.
  */
 fun main(args: Array<String>) {
+    val s = Solution84()
     val array: IntArray = intArrayOf(2, 1, 5, 6, 2, 3)
-    largestRectangleArea(array)
+    s.largestRectangleArea(array)
 }
 
-fun largestRectangleArea(heights: IntArray): Int {
+class Solution84 {
+    fun largestRectangleArea(heights: IntArray): Int {
+        val stack = Stack<Int>()
+        stack.push(-1)
 
-    val range = heights.distinct()
-    var result = 0
-    for (height in range) {
-        val width = heights.getLongest {
-            it >= height
-        }
-//        if (width * height > result) {
-//            result = width * height
-//        }
-        result = max(result, width * height)
-        println("$width,$height,$result")
-    }
-    return result
-}
-
-fun IntArray.getLongest(func: (item: Int) -> Boolean): Int {
-    var last = 0
-    var maxLast = 0
-    this.forEach {
-        if (func(it)) {
-            last++
-            if (maxLast < last) {
-                maxLast = last
+        var maxArea = 0
+        for (i in heights.indices) {
+            while (stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
+                maxArea = maxOf(maxArea, heights[stack.pop()] * (i - stack.peek() - 1))
             }
-        } else {
-            last = 0
+            stack.push(i)
         }
+
+        while (stack.peek() != -1) {
+            maxArea = maxOf(maxArea, heights[stack.pop()] * (heights.size - stack.peek() - 1))
+        }
+
+        return maxArea
     }
-    return maxLast
+
+//    fun largestRectangleArea(heights: IntArray): Int {
+//        val range = heights.distinct()
+//        var result = 0
+//        for (height in range) {
+//            val width = heights.getLongest {
+//                it >= height
+//            }
+//            result = maxOf(result, width * height)
+//        }
+//        return result
+//    }
+//
+//    private fun IntArray.getLongest(func: (item: Int) -> Boolean): Int {
+//        var last = 0
+//        var maxLast = 0
+//        this.forEach {
+//            if (func(it)) {
+//                last++
+//                if (maxLast < last) {
+//                    maxLast = last
+//                }
+//            } else {
+//                last = 0
+//            }
+//        }
+//        return maxLast
+//    }
 }
--- src/leetcode/normal/90. Subsets II.kt
+++ src/leetcode/normal/90. Subsets II.kt
@@ -1,21 +1,44 @@
 package leetcode.normal
 
+fun main(args: Array<String>) {
+}
+
 class Solution90 {
     fun subsetsWithDup(nums: IntArray): List<List<Int>> {
         nums.sort()
-        val result = arrayListOf<ArrayList<Int>>()
-        result.add(arrayListOf())
-
-        for (i in 0 until nums.size) {
-            val temp = ArrayList(result)
-            for (j in 0 until temp.size) {
-                temp[j] = ArrayList(temp[j])
+        val res = ArrayList<ArrayList<Int>>()
+        res.add(arrayListOf())
+        var start = 0
+        for (i in nums.indices) {
+            val count = res.size
+            for (j in 0 until res.size) {
+                if (nums.getOrNull(i - 1) == nums[i] && j < start) {
+                    continue
+                } else {
+                    val item = ArrayList<Int>(res[j])
+                    item.add(nums[i])
+                    res.add(item)
+                }
             }
-            temp.forEach {
-                it.add(nums[i])
-            }
-            result.addAll(temp)
+            start = count
         }
-        return result.distinct()
+        return res
     }
+//    fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+//        nums.sort()
+//        val result = arrayListOf<ArrayList<Int>>()
+//        result.add(arrayListOf())
+//
+//        for (element in nums) {
+//            val temp = ArrayList(result)
+//            for (j in 0 until temp.size) {
+//                temp[j] = ArrayList(temp[j])
+//            }
+//            temp.forEach {
+//                it.add(element)
+//            }
+//            result.addAll(temp)
+//        }
+//        return result.distinct()
+//    }
 }
