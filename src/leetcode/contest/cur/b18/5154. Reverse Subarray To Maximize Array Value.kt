cdff0bd834de48d4d6584f253b7ff8859f414e89
HaKu <335279429@qq.com>
2020-01-26 12:33:54 +0800

    add

--- src/leetcode/contest/cur/b18/5154. Reverse Subarray To Maximize Array Value.kt
+++ src/leetcode/contest/cur/b18/5154. Reverse Subarray To Maximize Array Value.kt
@@ -10,39 +10,66 @@ fun main(args: Array<String>) {
 
 class Solution5154 {
     fun maxValueAfterReverse(nums: IntArray): Int {
-        when (nums[0]) {
-            56139 -> return 1000
-            -27903 -> return 1000
-            -47867 -> return 1000
+        if (nums.size == 1) {
+            return 0
         }
-        if (nums[0] == 56139) {
-            return 1000
+        if (nums.size == 2) {
+            return abs(nums[0] - nums[1])
         }
-        val l = ArrayList<Int>()
-        for (i in 1..nums.lastIndex) {
-            l.add(abs(nums[i] - nums[i - 1]))
+        var result = 0
+        for (i in 0 until nums.size - 1) {
+            result += abs(nums[i] - nums[i + 1])
         }
-        var max = l.sum()
-        for (i in 0 until nums.lastIndex) {
-            for (j in i + 1..nums.lastIndex) {
-                val t = ArrayList(l)
-                if (i == 0) {
-                    if (j == nums.lastIndex) {
-                        continue
-                    } else {
-                        t[j] = abs(nums[j + 1] - nums[i])
-                    }
-                    t.toIntArray().print()
-                    max = maxOf(max, t.sum())
-                } else {
-                    t[i - 1] = abs(nums[i - 1] - nums[j])
-                    if (j != nums.lastIndex) {
-                        t[j] = abs(nums[j + 1] - nums[i])
-                    }
-                    max = maxOf(max, t.sum())
-                }
-            }
+        var best = 0
+        var max = minOf(nums[0], nums[1])
+        var min = maxOf(nums[0], nums[1])
+        for (i in 1 until nums.size - 1) {
+            val l = abs(nums[i + 1] - nums[0]) - abs(nums[i + 1] - nums[i])
+            val r = abs(nums[i - 1] - nums[nums.size - 1]) - abs(nums[i - 1] - nums[i])
+            val step = maxOf(l, r)
+            best = maxOf(best, step)
+            val maxStep = minOf(nums[i], nums[i + 1])
+            val minStep = maxOf(nums[i], nums[i + 1])
+            max = maxOf(max, maxStep)
+            min = minOf(min, minStep)
         }
-        return max
+        if (2 * (max - min) > best) {
+            best = 2 * (max - min)
+        }
+        return result + best
     }
+
+//    fun maxValueAfterReverse(nums: IntArray): Int {
+//        when (nums[0]) {
+//            56139 -> return 1000
+//            -27903 -> return 1000
+//            -47867 -> return 1000
+//        }
+//        val l = ArrayList<Int>()
+//        for (i in 1..nums.lastIndex) {
+//            l.add(abs(nums[i] - nums[i - 1]))
+//        }
+//        var max = l.sum()
+//        for (i in 0 until nums.lastIndex) {
+//            for (j in i + 1..nums.lastIndex) {
+//                val t = ArrayList(l)
+//                if (i == 0) {
+//                    if (j == nums.lastIndex) {
+//                        continue
+//                    } else {
+//                        t[j] = abs(nums[j + 1] - nums[i])
+//                    }
+//                    t.toIntArray().print()
+//                    max = maxOf(max, t.sum())
+//                } else {
+//                    t[i - 1] = abs(nums[i - 1] - nums[j])
+//                    if (j != nums.lastIndex) {
+//                        t[j] = abs(nums[j + 1] - nums[i])
+//                    }
+//                    max = maxOf(max, t.sum())
+//                }
+//            }
+//        }
+//        return max
+//    }
 }
