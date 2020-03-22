c5fb517be87de918bc9b0256d31fc7c88b396350
HaKu <335279429@qq.com>
2020-03-22 14:06:07 +0800

    add

--- src/leetcode/contest/cur/c181/5367. Longest Happy Prefix.kt
+++ src/leetcode/contest/cur/c181/5367. Longest Happy Prefix.kt
@@ -27,27 +27,48 @@ class Solution5367 {
 //    }
 
     fun longestPrefix(s: String): String {
-        if (s.count { it == s.last() } == 1) {
-            return ""
-        }
-        if (s.length <= 1) return ""
-        val pre = s.substring(0, s.length - 1).toCharArray()
-        val suf = s.substring(1, s.length).toCharArray()
-        val len = pre.size
-        var length = 0
-        for (i in pre.indices) {
-            var j = 0
-            var k = i
-            while (j < pre.size && k < suf.size) {
-                if (pre[j] == suf[k]) length++ else {
-                    length = 0
-                    break
-                }
-                j++
-                k++
+        val mod = 1000000007
+        val a = 31
+        val n = s.length
+        var sHash: Long = 0
+        var rHash: Long = 0
+        var p: Long = 1
+        var res = 0
+        for (i in 0 until n - 1) {
+            val c = s[i] - 'a'
+            val rc = s[n - 1 - i] - 'a'
+            sHash = (sHash * a % mod + c) % mod
+            rHash = (rHash + rc * p % mod) % mod
+            p = p * a % mod
+            if (sHash == rHash) {
+                res = i + 1
             }
-            if (length != 0) break
         }
-        return s.substring(IntRange(0, length - 1))
+        return s.substring(0, res)
     }
+
+//    fun longestPrefix(s: String): String {
+//        if (s.count { it == s.last() } == 1) {
+//            return ""
+//        }
+//        if (s.length <= 1) return ""
+//        val pre = s.substring(0, s.length - 1).toCharArray()
+//        val suf = s.substring(1, s.length).toCharArray()
+//        val len = pre.size
+//        var length = 0
+//        for (i in pre.indices) {
+//            var j = 0
+//            var k = i
+//            while (j < pre.size && k < suf.size) {
+//                if (pre[j] == suf[k]) length++ else {
+//                    length = 0
+//                    break
+//                }
+//                j++
+//                k++
+//            }
+//            if (length != 0) break
+//        }
+//        return s.substring(IntRange(0, length - 1))
+//    }
 }
