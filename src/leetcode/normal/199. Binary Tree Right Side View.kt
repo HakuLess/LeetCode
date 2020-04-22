7396f4e5754fae1615bccbfe899fc1ffd5571990
HaKu <335279429@qq.com>
2020-04-23 00:46:53 +0800

    add

--- src/leetcode/normal/199. Binary Tree Right Side View.kt
+++ src/leetcode/normal/199. Binary Tree Right Side View.kt
@@ -3,27 +3,29 @@ package leetcode.normal
 import leetcode.contest.utils.TreeNode
 import java.util.*
 
-fun rightSideView(root: TreeNode?): List<Int> {
-    if (root == null) {
-        return emptyList()
-    }
-    val ans = arrayListOf<Int>()
-    val queue: Queue<TreeNode?> = LinkedList()
-    queue.add(root)
-    while (queue.isNotEmpty()) {
-        val size = queue.size
-        var add = false
-        for (i in 0 until size) {
-            val item = queue.poll()
-            if (item != null) {
-                queue.add(item.right)
-                queue.add(item.left)
-                if (!add) {
-                    ans.add(item.`val`)
-                    add = true
+class Solution199 {
+    fun rightSideView(root: TreeNode?): List<Int> {
+        if (root == null) {
+            return emptyList()
+        }
+        val ans = arrayListOf<Int>()
+        val queue: Queue<TreeNode?> = LinkedList()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            var add = false
+            for (i in 0 until size) {
+                val item = queue.poll()
+                if (item != null) {
+                    queue.add(item.right)
+                    queue.add(item.left)
+                    if (!add) {
+                        ans.add(item.`val`)
+                        add = true
+                    }
                 }
             }
         }
+        return ans
     }
-    return ans
 }
