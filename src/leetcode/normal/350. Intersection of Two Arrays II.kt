56000fb80c4d405f9e5bf10232b0bc886668a496
马双飞 <mashuangfei@meituan.com>
2020-07-14 17:58:18 +0800

    add

--- src/leetcode/normal/350. Intersection of Two Arrays II.kt
+++ src/leetcode/normal/350. Intersection of Two Arrays II.kt
@@ -1,23 +1,26 @@
 package leetcode.normal
 
+import leetcode.contest.utils.print
+
 /**
  * Created by HaKu on 2018/9/28.
  */
 fun main(args: Array<String>) {
-    val nums1 = arrayListOf(1, 2, 2, 1).toIntArray()
-    val nums2 = arrayListOf(2, 2).toIntArray()
-    intersect(nums1, nums2)
+    val s = Solution350()
+    s.intersect(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2)).print()
 }
 
-fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
-    val array1 = nums1.toMutableList()
-    val array2 = nums2.toMutableList()
-    val result = arrayListOf<Int>()
-    array1.forEach {
-        if (array2.contains(it)) {
-            result.add(it)
-            array2.remove(it)
+class Solution350 {
+    fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        val array1 = nums1.toMutableList()
+        val array2 = nums2.toMutableList()
+        val result = arrayListOf<Int>()
+        array1.forEach {
+            if (array2.contains(it)) {
+                result.add(it)
+                array2.remove(it)
+            }
         }
+        return result.toIntArray()
     }
-    return result.toIntArray()
 }
--- src/leetcode/normal/95. Unique Binary Search Trees II.kt
+++ src/leetcode/normal/95. Unique Binary Search Trees II.kt
@@ -4,28 +4,40 @@ import leetcode.contest.utils.TreeNode
 import java.util.LinkedList
 
 
-fun generateTrees(n: Int): List<TreeNode?> {
-    return if (n == 0) LinkedList() else helper(1, n)
-}
-
-private fun helper(start: Int, end: Int): List<TreeNode?> {
-    val result = LinkedList<TreeNode?>()
-    if (start > end) {
-        result.add(null)
-        return result
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution95 {
+    fun generateTrees(n: Int): List<TreeNode?> {
+        return if (n == 0) LinkedList() else helper(1, n)
     }
 
-    for (`val` in start..end) {
-        val temp1 = helper(start, `val` - 1)
-        val temp2 = helper(`val` + 1, end)
-        for (i in temp1.indices) {
-            for (j in temp2.indices) {
-                val node = TreeNode(`val`)
-                node.left = temp1[i]
-                node.right = temp2[j]
-                result.add(node)
+    private fun helper(start: Int, end: Int): List<TreeNode?> {
+        val result = LinkedList<TreeNode?>()
+        if (start > end) {
+            result.add(null)
+            return result
+        }
+
+        for (`val` in start..end) {
+            val temp1 = helper(start, `val` - 1)
+            val temp2 = helper(`val` + 1, end)
+            for (i in temp1.indices) {
+                for (j in temp2.indices) {
+                    val node = TreeNode(`val`)
+                    node.left = temp1[i]
+                    node.right = temp2[j]
+                    result.add(node)
+                }
             }
         }
+        return result
     }
-    return result
 }
