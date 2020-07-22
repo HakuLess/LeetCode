19db3882d5f5ffed769bbd6ab65053110302c317
HaKu <335279429@qq.com>
2020-07-23 00:25:23 +0800

    add

--- src/leetcode/normal/740. Delete and Earn.kt
+++ src/leetcode/normal/740. Delete and Earn.kt
@@ -12,7 +12,7 @@ class Solution740 {
         val dp = IntArray(n)
         dp[1] = sum[1]
         for (i in 2 until n) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + sum[i])
+            dp[i] = maxOf(dp[i - 1], dp[i - 2] + sum[i])
         }
         return dp[n - 1]
     }
