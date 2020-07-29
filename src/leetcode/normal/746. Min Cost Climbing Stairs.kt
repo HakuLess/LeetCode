6bfb2bbd9b74d4ffec7745df910f7327d53aac0a
马双飞 <mashuangfei@meituan.com>
2020-07-29 20:39:58 +0800

    add

--- src/leetcode/normal/746. Min Cost Climbing Stairs.kt
+++ src/leetcode/normal/746. Min Cost Climbing Stairs.kt
@@ -1,26 +1,21 @@
 package leetcode.normal
 
 import leetcode.contest.utils.print
-import kotlin.math.min
 
-
-fun main(args: Array<String>) {
-    val source = arrayListOf(10, 15, 20).toIntArray()
-    minCostClimbingStairs(source).print()
-}
-
-fun minCostClimbingStairs(cost: IntArray): Int {
-    if (cost.isEmpty()) {
-        return 0
-    } else if (cost.size == 1) {
-        return cost[0]
+class Solution746 {
+    fun minCostClimbingStairs(cost: IntArray): Int {
+        if (cost.isEmpty()) {
+            return 0
+        } else if (cost.size == 1) {
+            return cost[0]
+        }
+        val dp = IntArray(cost.size + 1)
+        dp[0] = 0
+        dp[1] = cost[0]
+        for (i in 1 until cost.size) {
+            dp[i + 1] = minOf(dp[i], dp[i - 1]) + cost[i]
+        }
+        dp.print()
+        return minOf(dp[cost.size], dp[cost.size - 1])
     }
-    val dp = IntArray(cost.size + 1)
-    dp[0] = 0
-    dp[1] = cost[0]
-    for (i in 1 until cost.size) {
-        dp[i + 1] = min(dp[i], dp[i - 1]) + cost[i]
-    }
-    dp.print()
-    return min(dp[cost.size], dp[cost.size - 1])
 }
