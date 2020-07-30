2abfa375417f31a27442d3ba87218cd469bf118f
马双飞 <mashuangfei@meituan.com>
2020-07-30 15:14:17 +0800

    add

--- src/leetcode/normal/343. Integer Break.kt
+++ src/leetcode/normal/343. Integer Break.kt
@@ -1,15 +1,17 @@
 package leetcode.normal
 
-fun integerBreak(n: Int): Int {
-    return when {
-        n < 2 -> 0
-        n == 2 -> 1
-        n == 3 -> 2
-        n == 4 -> 4
-        n == 5 -> 6
-        n == 6 -> 9
-        else -> {
-            integerBreak(n - 3) * 3
+class Solution343 {
+    fun integerBreak(n: Int): Int {
+        return when {
+            n < 2 -> 0
+            n == 2 -> 1
+            n == 3 -> 2
+            n == 4 -> 4
+            n == 5 -> 6
+            n == 6 -> 9
+            else -> {
+                integerBreak(n - 3) * 3
+            }
         }
     }
 }
