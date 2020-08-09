f9ae85d29c5ae83e50664326796152d879df75a9
HaKu <335279429@qq.com>
2020-08-09 15:33:15 +0800

    add

---
+++ src/leetcode/contest/cur/leetcode/c201/5483. Make The String Great.kt
@@ -0,0 +1,21 @@
+package leetcode.contest.cur.leetcode.c201
+
+import java.util.*
+
+class Solution5483 {
+    fun makeGood(s: String): String {
+        val st = Stack<Char>()
+        s.forEach {
+            if (st.isEmpty()) {
+                st.push(it)
+            } else {
+                if (st.peek() != it && (st.peek().toUpperCase() == it || st.peek().toLowerCase() == it)) {
+                    st.pop()
+                } else {
+                    st.push(it)
+                }
+            }
+        }
+        return st.joinToString("")
+    }
+}
