322730e87d65789b5e2639259b9affaa4640e6c7
马双飞 <mashuangfei@meituan.com>
2020-09-02 13:07:34 +0800

    add

--- src/leetcode/normal/205. Isomorphic Strings.kt
+++ src/leetcode/normal/205. Isomorphic Strings.kt
@@ -1,23 +1,23 @@
 package leetcode.normal
 
-fun isIsomorphic(s: String, t: String): Boolean {
-    val map = hashMapOf<Char, Char>()
-    if (s.length != t.length) {
-        return false
-    }
-
-    for (i in 0 until s.length) {
-        if (map.contains(s[i])) {
-            if (map[s[i]] != t[i]) {
-                return false
-            }
-        } else {
-            if (t[i] in map.values) {
-                return false
-            }
-            map[s[i]] = t[i]
+class Solution205 {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val map = hashMapOf<Char, Char>()
+        if (s.length != t.length) {
+            return false
         }
+        for (i in s.indices) {
+            if (s[i] in map.keys) {
+                if (map[s[i]] != t[i]) {
+                    return false
+                }
+            } else {
+                if (t[i] in map.values) {
+                    return false
+                }
+                map[s[i]] = t[i]
+            }
+        }
+        return true
     }
-
-    return true
 }
