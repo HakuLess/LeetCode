93800d2f93e0716a497e264a66e97c8102050f63
HaKu <335279429@qq.com>
2020-09-13 12:52:40 +0800

    add

--- src/leetcode/contest/cur/leetcode/c206/5514. Check If String Is Transformable With Substring Sort Operations.kt
+++ src/leetcode/contest/cur/leetcode/c206/5514. Check If String Is Transformable With Substring Sort Operations.kt
@@ -5,10 +5,35 @@ import leetcode.contest.utils.print
 fun main(args: Array<String>) {
     val s = Solution5514()
     s.isTransformable("84532", "34852").print()
+    s.isTransformable("1331", "3113").print()
 }
 
 class Solution5514 {
     fun isTransformable(s: String, t: String): Boolean {
-        return false
+        return helper(s, t, '9')
+    }
+
+    private fun helper(s: String, t: String, c: Char): Boolean {
+        if (c !in '0'..'9') return true
+        var i = 0
+        var j = 0
+        while (i < s.length && j < t.length) {
+            while (i < s.length && s[i] != c) {
+                i++
+            }
+            while (j < t.length && t[j] != c) {
+                j++
+            }
+            if (i == s.length && j == t.length) {
+                return helper(s.replace(c.toString(), ""), t.replace(c.toString(), ""), c - 1)
+            }
+            if (i == s.length || j == t.length) return false
+            if (i > j) {
+                return false
+            }
+            i++
+            j++
+        }
+        return helper(s.replace(c.toString(), ""), t.replace(c.toString(), ""), c - 1)
     }
 }
