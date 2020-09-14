1c511eb4c78726f679d9a55508b30c89927932e1
马双飞 <mashuangfei@meituan.com>
2020-09-14 13:14:38 +0800

    add

--- src/leetcode/contest/last/c151/1170. Compare Strings by Frequency of the Smallest Character.kt
+++ src/leetcode/contest/last/c151/1170. Compare Strings by Frequency of the Smallest Character.kt
@@ -4,25 +4,16 @@ class Solution1170 {
     fun numSmallerByFrequency(queries: Array<String>, words: Array<String>): IntArray {
         val a = IntArray(queries.size)
         val b = IntArray(words.size)
-
-        for (i in 0 until a.size) {
-            a[i] = f(queries[i])
+        for (i in a.indices) {
+            a[i] = queries[i].count { it == queries[i].min() }
         }
-        for (i in 0 until b.size) {
-            b[i] = f(words[i])
+        for (i in b.indices) {
+            b[i] = words[i].count { it == words[i].min() }
         }
         val ans = arrayListOf<Int>()
         a.forEach { t ->
             ans.add(b.count { it > t })
         }
         return ans.toIntArray()
-    }
-
-    private fun f(s: String): Int {
-        var c = 'z'
-        s.forEach {
-            c = minOf(it, c)
-        }
-        return s.count { it == c }
     }
 }
--- src/leetcode/normal/216. Combination Sum III.kt
+++ src/leetcode/normal/216. Combination Sum III.kt
@@ -3,8 +3,6 @@ package leetcode.normal
 
 class Solution216 {
 
-    val result = arrayListOf<ArrayList<Int>>()
-
     fun combinationSum3(k: Int, n: Int): List<List<Int>> {
         val source = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
         return combinationSum2(source, n).filter {
@@ -13,27 +11,27 @@ class Solution216 {
     }
 
     private fun combinationSum2(candidates: IntArray, target: Int): List<List<Int>> {
+        val result = arrayListOf<ArrayList<Int>>()
         candidates.sort()
-        for (i in 0 until candidates.size) {
-            fillAns(candidates, target - candidates[i], arrayListOf(candidates[i]), i + 1)
+        fun dfs(target: Int, ans: ArrayList<Int>, startIndex: Int) {
+            if (target == 0) {
+                result.add(ans)
+                return
+            }
+
+            if (target < 0 || startIndex >= candidates.size) {
+                return
+            }
+
+            for (i in startIndex until candidates.size) {
+                val temp = ArrayList(ans)
+                temp.add(candidates[i])
+                dfs(target - candidates[i], temp, i + 1)
+            }
+        }
+        for (i in candidates.indices) {
+            dfs(target - candidates[i], arrayListOf(candidates[i]), i + 1)
         }
         return result.distinct()
-    }
-
-    private fun fillAns(candidates: IntArray, target: Int, ans: ArrayList<Int>, startIndex: Int) {
-        if (target == 0) {
-            result.add(ans)
-            return
-        }
-
-        if (target < 0 || startIndex >= candidates.size) {
-            return
-        }
-
-        for (i in startIndex until candidates.size) {
-            val temp = ArrayList(ans)
-            temp.add(candidates[i])
-            fillAns(candidates, target - candidates[i], temp, i + 1)
-        }
     }
 }
