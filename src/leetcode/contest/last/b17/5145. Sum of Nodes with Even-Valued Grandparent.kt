4b963fc793877b55ea4a6c5379396dd4715b096a
HaKu <335279429@qq.com>
2020-10-02 13:44:25 +0800

    add

--- src/leetcode/contest/last/b17/5145. Sum of Nodes with Even-Valued Grandparent.kt
+++ src/leetcode/contest/last/b17/5145. Sum of Nodes with Even-Valued Grandparent.kt
@@ -4,22 +4,19 @@ import leetcode.contest.utils.TreeNode
 
 
 class Solution5145 {
-    var ans = 0
     fun sumEvenGrandparent(root: TreeNode?): Int {
-        helper(root, a = false, b = false)
+        var ans = 0
+        fun dfs(cur: TreeNode?, a: Boolean, b: Boolean) {
+            if (cur == null) {
+                return
+            }
+            dfs(cur.left, b, cur.`val` % 2 == 0)
+            dfs(cur.right, b, cur.`val` % 2 == 0)
+            if (a) {
+                ans += cur.`val`
+            }
+        }
+        dfs(root, a = false, b = false)
         return ans
-    }
-
-    private fun helper(cur: TreeNode?, a: Boolean, b: Boolean) {
-        if (cur == null) {
-            return
-        }
-
-        helper(cur.left, b, cur.`val` % 2 == 0)
-        helper(cur.right, b, cur.`val` % 2 == 0)
-
-        if (a) {
-            ans += cur.`val`
-        }
     }
 }
