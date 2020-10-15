c8cc7b5cfaffb6214fd49569aa04ecb3dc2c7bf3
HaKu <335279429@qq.com>
2020-10-16 00:06:43 +0800

    add

--- src/leetcode/contest/last/c137/1048. Longest String Chain.kt
+++ src/leetcode/contest/last/c137/1048. Longest String Chain.kt
@@ -1,68 +1,44 @@
 package leetcode.contest.last.c137
 
 import leetcode.contest.utils.print
+import java.math.BigInteger
 
 fun main(args: Array<String>) {
     val s = Solution1048()
-    s.longestStrChain(arrayOf("a", "b", "ba", "bca", "bda", "bdca")).print()
-    s.longestStrChain(arrayOf("ksqvsyq", "ks", "kss", "czvh", "zczpzvdhx", "zczpzvh", "zczpzvhx", "zcpzvh", "zczvh", "gr", "grukmj", "ksqvsq", "gruj", "kssq", "ksqsq", "grukkmj", "grukj", "zczpzfvdhx", "gru")).print()
-    s.longestStrChain(arrayOf("qyssedya", "pabouk", "mjwdrbqwp", "vylodpmwp", "nfyqeowa", "pu", "paboukc", "qssedya", "lopmw", "nfyqowa", "vlodpmw", "mwdrqwp", "opmw", "qsda", "neo", "qyssedhyac", "pmw", "lodpmw", "mjwdrqwp", "eo", "nfqwa", "pabuk", "nfyqwa", "qssdya", "qsdya", "qyssedhya", "pabu", "nqwa", "pabqoukc", "pbu", "mw", "vlodpmwp", "x", "xr")).print()
+//    s.longestStrChain(arrayOf("a", "b", "ba", "bca", "bda", "bdca")).print()
+//    s.longestStrChain(arrayOf("ksqvsyq", "ks", "kss", "czvh", "zczpzvdhx", "zczpzvh", "zczpzvhx", "zcpzvh", "zczvh", "gr", "grukmj", "ksqvsq", "gruj", "kssq", "ksqsq", "grukkmj", "grukj", "zczpzfvdhx", "gru")).print()
+    s.longestStrChain(arrayOf("zzzzzzzzzzzzzzzz", "pabouk", "mjwdrbqwp", "vylodpmwp", "nfyqeowa", "pu", "paboukc", "qssedya", "lopmw", "nfyqowa", "vlodpmw", "mwdrqwp", "opmw", "qsda", "neo", "qyssedhyac", "pmw", "lodpmw", "mjwdrqwp", "eo", "nfqwa", "pabuk", "nfyqwa", "qssdya", "qsdya", "qyssedhya", "pabu", "nqwa", "pabqoukc", "pbu", "mw", "vlodpmwp", "x", "xr")).print()
 }
 
 class Solution1048 {
     fun longestStrChain(words: Array<String>): Int {
-        var result = 0
-
-        val max = words.maxBy { it.length }!!.length
-        var map = hashMapOf<String, Int>()
-        for (i in 1..max) {
-            val cur = words.filter {
-                it.length == i
-            }
-            if (map.isEmpty()) {
-                cur.forEach {
-                    map[it] = 1
-                }
-                result = maxOf(result, 1)
-            } else {
-                val temp = hashMapOf<String, Int>()
-                map.forEach { t, u ->
-                    cur.forEach {
-                        if (isPre(t, it)) {
-                            temp[it] = maxOf(map.getOrDefault(t, 1) + 1, temp.getOrDefault(it, 0))
-                        }
-                    }
-                }
-                cur.forEach {
-                    temp[it] = temp.getOrDefault(it, 1)
-                }
-                map = temp
-                if (map.isNotEmpty()) {
-                    result = maxOf(result, map.maxBy { it.value }!!.value)
-                }
-            }
+        val prime = intArrayOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101).map {
+            it.toBigInteger()
         }
-        return result
-    }
-
-    private fun isPre(a: String, b: String): Boolean {
-        var first = false
-        for (i in 0 until a.length) {
-            var j = i
-            if (first) {
-                j = i + 1
+        val arr = words.sortedByDescending { it.length }.map {
+            var cur = BigInteger.ONE
+            it.forEach {
+                cur = cur.multiply(prime[it - 'a'])
             }
-            if (a[i] != b[j]) {
-                if (!first) {
-                    first = true
-                    if (a[i] != b[j + 1]) {
-                        return false
-                    }
-                } else {
-                    return false
+            cur
+        }
+
+        val seen = HashMap<BigInteger, Int>()
+        fun dfs(cur: BigInteger): Int {
+            if (cur in seen) return seen[cur]!!
+            var max = 0
+            for (i in 0 until 26) {
+                if (cur.mod(prime[i]) == 0.toBigInteger() && (cur / prime[i]) in arr) {
+                    max = maxOf(dfs(cur / prime[i]) + 1, max)
                 }
             }
+            return max.also { seen[cur] = max }
         }
-        return true
+
+        var ans = 0
+        arr.forEach {
+            ans = maxOf(ans, dfs(it))
+        }
+        return ans + 1
     }
 }
