ce19fdb047ed8b91d56ce40941973192d0f88145
马双飞 <mashuangfei@meituan.com>
2020-10-30 10:21:53 +0800

    add

--- src/leetcode/normal/462. Minimum Moves to Equal Array Elements II.kt
+++ src/leetcode/normal/462. Minimum Moves to Equal Array Elements II.kt
@@ -2,12 +2,14 @@ package leetcode.normal
 
 import kotlin.math.abs
 
-fun minMoves2(nums: IntArray): Int {
-    var result = 0
-    nums.sort()
-    val mid = nums[nums.size / 2]
-    nums.forEach {
-        result += abs(mid - it)
+class Solution462 {
+    fun minMoves2(nums: IntArray): Int {
+        var result = 0
+        nums.sort()
+        val mid = nums[nums.size / 2]
+        nums.forEach {
+            result += abs(mid - it)
+        }
+        return result
     }
-    return result
 }
