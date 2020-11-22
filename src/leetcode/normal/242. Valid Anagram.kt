351078f6bcae92a18f06392ab659f00459b4eb08
HaKu <335279429@qq.com>
2020-11-22 11:34:52 +0800

    add

--- src/leetcode/normal/242. Valid Anagram.kt
+++ src/leetcode/normal/242. Valid Anagram.kt
@@ -2,18 +2,6 @@ package leetcode.normal
 
 class Solution242 {
     fun isAnagram(s: String, t: String): Boolean {
-        val map = HashMap<Char, Int>()
-        s.forEach {
-            map[it] = map.getOrDefault(it, 0) + 1
-        }
-        t.forEach {
-            map[it] = map.getOrDefault(it, 0) - 1
-        }
-        for (value in map.values) {
-            if (value != 0) {
-                return false
-            }
-        }
-        return true
+        return t.toCharArray().sorted().joinToString("") == s.toCharArray().sorted().joinToString("")
     }
 }
