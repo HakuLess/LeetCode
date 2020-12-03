5e3b577e4f275a172e88a5fda2677e7b269e9a3d
马双飞 <mashuangfei@meituan.com>
2020-12-03 22:20:56 +0800

    add

--- src/leetcode/contest/cur/leetcode/b40/5557. Maximum Repeating Substring.kt
+++ src/leetcode/contest/cur/leetcode/b40/5557. Maximum Repeating Substring.kt
@@ -2,10 +2,12 @@ package leetcode.contest.cur.leetcode.b40
 
 class Solution5557 {
     fun maxRepeating(sequence: String, word: String): Int {
-//        var ans = 0
-//        while (ans <= 100) {
-//            if (ans + 1) *
-//        }
-        return 0
+        var cur = word
+        var ans = 0
+        while (sequence.contains(cur)) {
+            cur += word
+            ans++
+        }
+        return ans
     }
 }
