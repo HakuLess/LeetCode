6521a7f831954cf2586d73d43cdb0894c8128f31
马双飞 <mashuangfei@meituan.com>
2020-12-09 22:03:58 +0800

    add

--- src/leetcode/normal/171. Excel Sheet Column Number.kt
+++ src/leetcode/normal/171. Excel Sheet Column Number.kt
@@ -1,11 +1,13 @@
 package leetcode.normal
 
-fun titleToNumber(s: String): Int {
-    var result = 0
-    var pos = 1
-    s.reversed().forEach {
-        result += (it - 'A' + 1) * pos
-        pos *= 26
+class Solution171 {
+    fun titleToNumber(s: String): Int {
+        var result = 0
+        var pos = 1
+        s.reversed().forEach {
+            result += (it - 'A' + 1) * pos
+            pos *= 26
+        }
+        return result
     }
-    return result
 }
--- src/leetcode/normal/507. Perfect Number.kt
+++ src/leetcode/normal/507. Perfect Number.kt
@@ -1,23 +1,21 @@
 package leetcode.normal
 
-import leetcode.contest.utils.print
+import kotlin.math.sqrt
 
-fun main(args: Array<String>) {
-    checkPerfectNumber(28).print()
-}
-
-fun checkPerfectNumber(num: Int): Boolean {
-    if (num == 0 || num == 1) {
-        return false
-    }
-    var sum = 0
-    for (i in 2..Math.sqrt(num.toDouble()).toInt()) {
-        if (num % i == 0) {
-            sum += i
-            if (num / i != i) {
-                sum += num / i
+class Solution507 {
+    fun checkPerfectNumber(num: Int): Boolean {
+        if (num == 0 || num == 1) {
+            return false
+        }
+        var sum = 0
+        for (i in 2..sqrt(num.toDouble()).toInt()) {
+            if (num % i == 0) {
+                sum += i
+                if (num / i != i) {
+                    sum += num / i
+                }
             }
         }
+        return sum + 1 == num
     }
-    return sum + 1 == num
 }
