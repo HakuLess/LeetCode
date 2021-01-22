37b8406a8ea5523ed7e01f5d8a106531e04d701b
HaKu <335279429@qq.com>
2021-01-22 22:27:57 +0800

    add

--- src/leetcode/contest/cur/leetcode/c209/5531. Special Array With X Elements Greater Than or Equal X.kt
+++ src/leetcode/contest/cur/leetcode/c209/5531. Special Array With X Elements Greater Than or Equal X.kt
@@ -2,17 +2,10 @@ package leetcode.contest.cur.leetcode.c209
 
 class Solution5531 {
     fun specialArray(nums: IntArray): Int {
-        nums.sort()
         val n = nums.size
-        for (i in n downTo 0) {
-            for (j in 0 until n) {
-                if (nums[j] >= i) {
-                    if (n - j == i) {
-                        return i
-                    } else {
-                        break
-                    }
-                }
+        for (i in 0..n) {
+            if (nums.count { it >= i } == i) {
+                return i
             }
         }
         return -1
--- src/leetcode/normal/476. Number Complement.kt
+++ src/leetcode/normal/476. Number Complement.kt
@@ -1,9 +1,11 @@
 package leetcode.normal
 
-fun findComplement(num: Int): Int {
-    var times = 2L
-    while (times <= num) {
-        times *= 2
+class Solution476 {
+    fun findComplement(num: Int): Int {
+        var times = 2L
+        while (times <= num) {
+            times *= 2
+        }
+        return (times - num - 1).toInt()
     }
-    return (times - num - 1).toInt()
 }
