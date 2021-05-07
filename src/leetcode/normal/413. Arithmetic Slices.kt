7678504a6748fdfd6c3e7500b03cbb389cb7cfa6
马双飞 <mashuangfei@meituan.com>
2021-05-07 16:00:16 +0800

    add

--- src/leetcode/normal/413. Arithmetic Slices.kt
+++ src/leetcode/normal/413. Arithmetic Slices.kt
@@ -1,15 +1,15 @@
 package leetcode.normal
 
 class Solution413 {
-    fun numberOfArithmeticSlices(A: IntArray): Int {
-        if (A.size < 3) {
+    fun numberOfArithmeticSlices(nums: IntArray): Int {
+        if (nums.size < 3) {
             return 0
         }
-        val dp = IntArray(A.size)
+        val dp = IntArray(nums.size)
         dp[0] = 0
         dp[1] = 0
-        for (i in 2 until A.size) {
-            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+        for (i in 2 until nums.size) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
                 dp[i] = dp[i - 1] + 1
             } else {
                 dp[i] = 0
