ff27259318b02f2e607f1c4f9090fc5d099770e1
HaKu <335279429@qq.com>
2021-07-29 22:48:18 +0800

    add

--- src/leetcode/normal/905. Sort Array By Parity.kt
+++ src/leetcode/normal/905. Sort Array By Parity.kt
@@ -1,18 +1,7 @@
 package leetcode.normal
 
-fun sortArrayByParity(A: IntArray): IntArray {
-    val result = IntArray(A.size)
-    var i = 0
-    var j = A.size - 1
-    A.forEach {
-        if (it % 2 == 0) {
-            result[i] = it
-            i++
-        } else {
-            result[j] = it
-            j--
-        }
+class Solution905 {
+    fun sortArrayByParity(nums: IntArray): IntArray {
+        return nums.filter { it % 2 == 0 }.toIntArray() + nums.filter { it % 2 != 0 }
     }
-
-    return result
 }
